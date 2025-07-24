1d5fd6e092a47ce07b46a296b0e38b396fac738a
xznnnnnnnnnnnnnnnn <sethcasseus1@gmail.com>
2025-07-24 15:30:46 -0400

    Update whitelist.js

--- whitelist.js
+++ whitelist.js
@@ -1,1 +1,6 @@
+module.exports = [
+  'floortooue',          // example username
+  'floortoouew',      // add actual Roblox usernames
+  'usmcTrainer01', // customize for your ranks or tryout leaders
+];
 
